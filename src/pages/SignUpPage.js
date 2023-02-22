1b8ecf8c6b4c938939e6b0488cda77aa8649edbc
Nghe Pham <npham@markany.com>
2023-02-22 15:05:11 +0700

    sign-up form html

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -2,57 +2,39 @@
 import React from 'react';
 import '../App.css';
 import './SignUpPage.css'
+import logo from '../assets/logo.svg';
 
 function SignUpPage() {
   return (
-    <div className={`dark`}>
-      <h1 style={{textAlign:'center'}}>Sign up</h1>
-      <form id='register-form'>
-        <div>
-          <p className='input-label'>Full name:</p>
-          <input className='input-item' type='text' name="full-name" />
-        </div>
-
-        <div>
-          <p className='input-label'>Email:</p>
-          <input className='input-item' type='email' name="email" />
-        </div>
-        <div>
-          <p className='input-label'>Password:</p>
-          <input className='input-item' type='password' name="password" />
-        </div>
-        <div>
-          <p className='input-label'>Confirm password:</p>
-          <input className='input-item' type='password' name="confirm-password" />
-        </div>
-        <div>
-          <p className='input-label'>Gender:</p>
-          <label className='input-label'><input type='radio' name='gender' value='male'/>Male</label>
-          <label className='input-label'><input type='radio' name='gender' value='female' />Female</label>
-          <label className='input-label'><input type='radio' name='gender' value='other' />Other</label>
-        </div>
-        <div>
-          <p className='input-label'>Security question:</p>
-          <select className='input-item' name="security-question">
-            <option value="first-pet">What is your first pet?</option>
-            <option value="first-job">What is your first job?</option>
-            <option value="nickname">What is your nickname?</option>
-          </select>
+    <div id={`sign-up-container`}>
+      <div className='container'>
+        <div className='title-center'>Register</div>
+        <div id='login-social'>
           <div>
-            <textarea className='input-item' name='answer' cols={30} rows={6}>
-
-            </textarea>
-          </div>
-          <div>
-            <p id='terms-checkbox'>
-              <input type={'checkbox'} name='terms-condition' value={'accept'} />I agree to accept terms and conditions
-            </p>
-          </div>
-          <div>
-            <input type={'button'} value='Register' />
+            <a href="#" class="fa fa-google"></a>
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-github"></a>
+            <a href="#" class="fa fa-linkedin"></a>
           </div>
         </div>
-      </form>
+        <form id='register-form'>
+          <div className='input-container'>
+            <input className='input-item' placeholder='Username' type='text' name="username" required />
+          </div>
+          <div className='input-container'>
+            <input className='input-item' placeholder='Email' type='email' name="email" required />
+          </div>
+          <div className='input-container'>
+            <input className='input-item' placeholder='Password' type='password' name="password" required />
+          </div>
+          <div className='input-container'>
+            <input className='input-item' type='password' placeholder='Confirm Password' name="confirm-password" required />
+          </div>
+          <div id='div-btn'>
+            <input id='btn-register' type={'button'} value='Register' />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
